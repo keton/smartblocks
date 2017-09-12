b27277e8aa356fa00868816ff1c12f67067384cd
Michal Lower <michal@smablo.com>
2017-09-12 20:09:01 +0200

    comments and cleanup

--- examples/genericDeviceTest.js
+++ examples/genericDeviceTest.js
@@ -4,6 +4,8 @@ smartblocks.GenericDevice.discover(function (instance) {
 
     console.log('GenericDevice.discover');
 
+    //setup event listeners for notifications
+
     instance.on("PTHTemperatureNotif", function (data) {
         console.log("PTHTemperatureNotif: " + data);
     });
@@ -16,8 +18,24 @@ smartblocks.GenericDevice.discover(function (instance) {
         console.log("PTHPressureNotif: " + data);
     });
 
+    //connect to the device and perform service initialization
     instance.connectAndSetUp(function (callback) {
         console.log('connectAndSetUp');
+
+        /*
+        //example read operation
+        instance.PTHService.readTemperature(function(error, data){
+            console.log("PTHService.readTemperature: ", data);
+        });
+
+        instance.PTHService.readHumidity(function(error, data){
+            console.log("PTHService.readHumidity: ", data);
+        });
+
+        instance.PTHService.readPressure(function(error, data){
+            console.log("PTHService.readPressure: ", data);
+        });
+        */
     });
     
 });
