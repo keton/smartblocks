ac0dd209f64059944161c150e8d5fef787bb7d71
Michal Lower <michal@smablo.com>
2017-09-12 19:39:06 +0200

    working prototype of PTH service

--- examples/genericDeviceTest.js
+++ examples/genericDeviceTest.js
@@ -1,11 +1,23 @@
-var smartblocks=require('../index.js');
+var smartblocks = require('../index.js');
 
-smartblocks.GenericDevice.discover(function(instance){
+smartblocks.GenericDevice.discover(function (instance) {
 
     console.log('GenericDevice.discover');
 
-    instance.connectAndSetUp(function(callback) {
+    instance.on("PTHTemperatureNotif", function (data) {
+        console.log("PTHTemperatureNotif: " + data);
+    });
+    
+    instance.on("PTHHumidityNotif", function (data) {
+        console.log("PTHHumidityNotif: " + data);
+    });
+    
+    instance.on("PTHPressureNotif", function (data) {
+        console.log("PTHPressureNotif: " + data);
+    });
+
+    instance.connectAndSetUp(function (callback) {
         console.log('connectAndSetUp');
-        
-      });
+    });
+    
 });
